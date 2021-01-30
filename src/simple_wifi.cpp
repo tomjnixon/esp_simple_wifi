2caf774cdeb38f03eaa1554f296cff06b2af3038
Thomas Nixon <tom@tomn.co.uk>
2021-01-30 11:50:19 +0000

    more complete initialisation of NVS
    copied from example; this is mainly useful to make upgrading devices
    with old flash versions less painful

--- src/simple_wifi.cpp
+++ src/simple_wifi.cpp
@@ -30,7 +30,14 @@ static esp_err_t event_handler(void *ctx, system_event_t *event) {
 }
 
 void simple_wifi::start() {
-  ESP_ERROR_CHECK(nvs_flash_init());
+  // initialise nvs
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
 
   tcpip_adapter_init();
   ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
