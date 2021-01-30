20153a67199540e0cb61151cb0f303ab582a4b2d
Thomas Nixon <tom@tomn.co.uk>
2021-01-30 11:59:15 +0000

    switch from deprecated esp_event_loop to esp_event

--- src/simple_wifi.cpp
+++ src/simple_wifi.cpp
@@ -1,5 +1,5 @@
 #include "simple_wifi.hpp"
-#include "esp_event_loop.h"
+#include "esp_event.h"
 #include "esp_log.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
@@ -8,25 +8,18 @@
 
 static const char *TAG = "simple_wifi";
 
-static esp_err_t event_handler(void *ctx, system_event_t *event) {
-  switch (event->event_id) {
-  case SYSTEM_EVENT_STA_START:
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
     esp_wifi_connect();
-    break;
-  case SYSTEM_EVENT_STA_GOT_IP:
-    ESP_LOGI(TAG, "got ip:%s",
-             ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
-    break;
-  case SYSTEM_EVENT_STA_DISCONNECTED: {
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+  } else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED) {
     ESP_LOGI(TAG, "disconnected; retrying connection to the AP");
     esp_wifi_connect();
-    ESP_LOGI(TAG, "connect to the AP fail\n");
-    break;
   }
-  default:
-    break;
-  }
-  return ESP_OK;
 }
 
 void simple_wifi::start() {
@@ -40,13 +33,19 @@ void simple_wifi::start() {
   ESP_ERROR_CHECK(ret);
 
   tcpip_adapter_init();
-  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
 
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   cfg.nvs_enable = false;
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
   ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
 
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, NULL));
+
+  // actually configure wifi
   const char *ssid = CONFIG_SIMPLE_WIFI_SSID;
   const char *password = CONFIG_SIMPLE_WIFI_PASSWORD;
 
