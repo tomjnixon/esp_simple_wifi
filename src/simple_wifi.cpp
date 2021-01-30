49847979e23c14ef9a29d873690ddbc11e7cb14c
Thomas Nixon <tom@tomn.co.uk>
2021-01-30 12:00:02 +0000

    esp-idf 4.2-style wifi initialisation

--- src/simple_wifi.cpp
+++ src/simple_wifi.cpp
@@ -32,8 +32,10 @@ void simple_wifi::start() {
   }
   ESP_ERROR_CHECK(ret);
 
-  tcpip_adapter_init();
+  // initialise event loop, wifi and event handler
+  ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
 
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   cfg.nvs_enable = false;
