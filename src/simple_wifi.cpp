e0487fae6fbc32ddb1c44c26c2f45589d10389e0
Thomas Nixon <tom@tomn.co.uk>
2021-08-29 14:04:59 +0100

    update for esp-idf 4.3

--- src/simple_wifi.cpp
+++ src/simple_wifi.cpp
@@ -60,7 +60,7 @@ void simple_wifi::start() {
   wifi_config.sta.pmf_cfg.required = false;
 
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
   ESP_ERROR_CHECK(esp_wifi_start());
 
